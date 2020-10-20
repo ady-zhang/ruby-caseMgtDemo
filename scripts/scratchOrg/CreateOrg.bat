cae0e60def878f6fee5dfe3987f91f9afc1efdb1
ady-zhang <ady.zhang@foxmail.com>
2020-10-20 16:47:11 +0800

    MyFirstRestAPIClass

--- scripts/scratchOrg/CreateOrg.bat
+++ scripts/scratchOrg/CreateOrg.bat
@@ -1,7 +1,7 @@
 @REM Execute in Windows using: .\scripts\scratchOrg\CreateOrg.bat
 @echo off
 echo "*** Creating scratch Org..."
-call sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --setalias devCaseDEX602 -d 30
+call sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --setalias devCaseMgt -d 30
 echo "*** Opening scratch Org..."
 call sfdx force:org:open
 
